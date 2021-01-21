b2f45a6229cfaf69ef2d8a676eb6866423942680
Uver999 <mruver@protonmail.com>
2021-01-21 12:03:13 -0800

    Update monitor names

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -8,7 +8,7 @@ function run {
 }
 
 run xrandr --output HDMI-1 --primary --rate 119.98
-run xrandr --output EDP-1 --same-as HDMI-1
+run xrandr --output eDP-1 --same-as HDMI-1
 xinput --set-prop "Logitech M570" "libinput Accel Speed" -1
 xinput --set-prop "TPPS/2 Elan TrackPoint" "Coordinate Transformation Matrix" 0.3 0 0 0 0.3 0 0 0 1
 xset mouse 0 0

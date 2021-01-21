fc6d3f83e36c178f5aa40b899c588e7281cdab94
Uver999 <mruver@protonmail.com>
2021-01-20 23:01:57 -0800

    Set internal monitor to mirror primary

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -7,7 +7,8 @@ function run {
 	fi
 }
 
-run xrandr --output HDMI1 --primary --rate 119.98
+run xrandr --output HDMI-1 --primary --rate 119.98
+run xrandr --output EDP-1 --same-as HDMI-1
 xinput --set-prop "Logitech M570" "libinput Accel Speed" -1
 xinput --set-prop "TPPS/2 Elan TrackPoint" "Coordinate Transformation Matrix" 0.3 0 0 0 0.3 0 0 0 1
 xset mouse 0 0

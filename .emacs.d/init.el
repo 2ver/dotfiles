a581353c19f285ea9c967f7f167b1307fe5b346c
Uver999 <71196144+2ver@users.noreply.github.com>
2021-02-22 15:32:11 -0800

    Add org mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,6 +5,7 @@
 		scroll-margin 99999
 		scroll-perserve-screen-position t
 		scroll-conservatively 0)		; Scrolloff behaviour
+(setq display-line-numbers-type 'relative)
 (tool-bar-mode -1)						; Disable toolbar
 (tooltip-mode -1)							; Disable tooltips
 (set-fringe-mode 10)						; Give some breathing room
@@ -13,6 +14,24 @@
 (setq ring-bell-function 'ignore)	; Turn off bell
 
 (set-face-attribute 'default nil :font "Iosevka Custom" :height 110) ; Set font
+
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Iosevka Custom" :height 110)
+
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 110 :weight 'regular)
+
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+		`(("." . ,user-temporary-file-directory)
+		  (,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+		(concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+		`((".*" ,user-temporary-file-directory t)))
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -25,8 +44,8 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "http://elpa.gnu.org/packages/")))
+								 ("org" . "https://orgmode.org/elpa/")
+								 ("elpa" . "http://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -75,7 +94,7 @@
   :custom ((doom-modeline-height 25)))
 
 (use-package doom-themes
-  :init (load-theme 'doom-dracula t))
+  :init (load-theme 'doom-lena t))
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -194,3 +213,58 @@
 
 ;(use-package forge
 ;  :after magit)
+
+;; Org Mode ----------------------------------
+
+(defun uver/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+
+(defun uver/org-font-setup ()
+  ;; Replace list hyphens with dots
+  (font-lock-add-keywords 'org-mode
+								  '(("^ *\\([-]\\) "
+									  (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+  
+ (dolist (face '((org-level-1 . 1.2)
+					  (org-level-2 . 1.1)
+					  (org-level-3 . 1.05)
+					  (org-level-4 . 1.0)
+					  (org-level-5 . 1.1)
+					  (org-level-6 . 1.1)
+					  (org-level-7 . 1.1)
+					  (org-level-8 . 1.1)))
+	(set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+  
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+  
+(use-package org
+  :hook (org-mode . uver/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+		  org-hide-emphasis-markers t)
+  (uver/org-font-setup))
+	 
+(use-package org-bullets
+	:after org
+	:hook (org-mode . org-bullets-mode)
+	:custom
+	(org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun uver/org-mode-visual-fill ()
+	(setq visual-fill-column-width 100
+			visual-fill-column-center-text t)
+	(visual-fill-column-mode 1))
+  
+(use-package visual-fill-column
+	 :hook (org-mode . uver/org-mode-visual-fill))
